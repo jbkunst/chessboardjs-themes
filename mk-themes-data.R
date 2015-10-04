231d767d13880f635571848cedf2dcb46d3a9415
Joshua <jbkunst@gmail.com>
2015-10-04 20:28:12 -0300

    1st commit

---
+++ mk-themes-data.R
@@ -0,0 +1,62 @@
+rm(list =ls())
+library("knitr")
+library("magrittr")
+library("plyr")
+library("tools")
+library("jsonlite")
+library("V8")  
+library("whisker")
+
+
+dir()
+
+folders <-  dir("chesspieces/", full.names = FALSE)
+folders
+
+dic <- llply(folders, function(folder){
+  # folder <- sample(folders, 1)
+    pieces <- dir(file.path("chesspieces", folder), full.names=FALSE)
+    uris <- llply(pieces, function(piece, foldert = folder){
+      # piece <- sample(pieces, size = 1)
+      image_uri(file.path("chesspieces", foldert, piece))
+    })
+    names(uris) <- file_path_sans_ext(pieces)
+    uris
+})
+
+names(dic) <- folders
+
+
+files <- dir("chesspieces/", recursive = TRUE, full.names = TRUE)
+
+js <- toJSON(dic, pretty = TRUE) 
+
+
+
+ct <- new_context()
+ct$assign("chssbrdths", JS(js))
+
+ct$get("chssbrdths['alpha']['wR']") == ct$get("chssbrdths")$alpha$wR
+
+
+js <- paste("var chssbrdths = ", js)
+writeLines(js, con = "chessboardjs.themes.data.js")
+
+
+fncs <- laply(folders, function(folder){
+  template <- "{{ name }}_theme = function(piece){ return chssbrdths[\"{{ name }}\"][piece][0]; };"
+  whisker.render(template, list(name = folder))
+  })
+
+ct$eval(JS(fncs[1]))
+
+ct$eval(JS("alpha_theme('bB')"))  == dic$alpha$bB
+
+writeLines(fncs, con = "chessboardjs.themes.js")
+
+
+# system("python -m SimpleHTTPServer 8080")
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Chessboardjs Themes
+
+A differents set of pieces themes for *chessboardjs*. No images requiered.
+
+## How to use
+
+1. Load *chessboard.js* and *chessboard.css* as usual.
+2. Load  *chessboardjs.themes.js* and *chessboardjs.themes.data.js*.
+3. Use them using *pieceTheme* configuration option.
+
+## Example Use
+
+```
+var board = ChessBoard('aboardiv', {
+  pieceTheme: symbol_theme,
+  position: 'start'
+});
+```
+
+## Themes
+
+1. Alpha `alpha_theme`
+1. Wikipedia `wikipedia_theme`
+1. USCF `uscf_theme`
+1. Symbol `symbol_theme`
+1. Dilen `dilena_theme`.
