ec6cbceee064566fbb5f20f23135d18511b0f944
Joshua <jbkunst@gmail.com>
2015-11-26 15:49:51 -0300

    change format of names

---
+++ chessboardjs-themes-board.js
@@ -0,0 +1,7 @@
+chess24_board_theme = ['#9E7863', '#633526'];
+metro_board_theme = ['#EFEFEF', '#FFFFFF'];
+leipzig_board_theme = ['#FFFFFF', '#E1E1E1'];
+wikipedia_board_theme = ['#D18B47', '#FFCE9E'];
+dilena_board_theme = ['#FFE5B6', '#B16228'];
+uscf_board_theme = ['#C3C6BE', '#727FA2'];
+symbol_board_theme = ['#FFFFFF', '#58AC8A'];
---
+++ chessboardjs-themes-pieces-fns.js
@@ -0,0 +1,8 @@
+alpha_piece_theme = function(piece){ return chsspieces["alpha"][piece][0]; };
+chess24_piece_theme = function(piece){ return chsspieces["chess24"][piece][0]; };
+dilena_piece_theme = function(piece){ return chsspieces["dilena"][piece][0]; };
+leipzig_piece_theme = function(piece){ return chsspieces["leipzig"][piece][0]; };
+metro_piece_theme = function(piece){ return chsspieces["metro"][piece][0]; };
+symbol_piece_theme = function(piece){ return chsspieces["symbol"][piece][0]; };
+uscf_piece_theme = function(piece){ return chsspieces["uscf"][piece][0]; };
+wikipedia_piece_theme = function(piece){ return chsspieces["wikipedia"][piece][0]; };
